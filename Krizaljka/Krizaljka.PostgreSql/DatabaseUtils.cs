fc8f2d7b9214a3b86f77d58b27e0e39317f2bb8e
Darko <darkorazvoj@gmail.com>
2026-06-16 16:03:02 -0400

    Changed db error texts

--- Krizaljka/Krizaljka.PostgreSql/DatabaseUtils.cs
+++ Krizaljka/Krizaljka.PostgreSql/DatabaseUtils.cs
@@ -7,8 +7,8 @@ internal class DatabaseUtils : IDatabaseUtils
     public string MapSqlStateToError(string? sqlState) =>
         sqlState switch
         {
-            "23503" => "ForeignKeyViolation",
-            "23505" => "UniqueKeyViolation",
+            "23503" => "MissingReferencedData", //ForeignKeyViolation
+            "23505" => "IdeticalRecordExits", //UniqueKeyViolation
             IDatabaseUtils.InvalidChangestampCode => "InvalidChangeStamp",
             "CC002" => "RecordNotFound",
             "CC003" => "ChangestampMissing",
